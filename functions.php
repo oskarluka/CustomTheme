1c5542a64182c845b11e41c3f38716a3f559c023
oskarluka <oskarluka@hotmail.com>
2017-03-22 14:28:04 +0000

    V0.1.1

--- functions.php
+++ functions.php
@@ -17,4 +17,27 @@ register_nav_menus(array(
 	'footer' => __('Footer Menu'),
 ));
 
+function get_top_ancestor_id() {
+
+	global $post;
+
+	if ($post->post_parent) {
+		$ancestors = array_reverse(get_post_ancestors($post->ID));
+		return $ancestors[0];
+	}
+
+	return $post->ID;
+
+}
+
+function has_children() {
+
+	global $post;
+
+	$pages =  get_pages('child_of=' . $post->ID);
+	return count($pages);
+
+}
+
+
 ?>
