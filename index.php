670b5cbe69530866be850a26267306e7b75509ec
oskarluka <oskarluka@hotmail.com>
2017-03-22 15:01:09 +0000

    V0.1.11

--- index.php
+++ index.php
@@ -7,9 +7,30 @@ if (have_posts()) :
 
 	<article class="post">
 		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+		<p class="post-info"><?php the_time('jS,F,y'); ?> | by <a href="get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in 
+		<?php
+
+			$categories = get_the_category();
+			$separator = ", ";
+			$output = '';
+
+			if ($categories) {
+
+				foreach ($categories as $category) {
+
+					$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
+				}
+
+				echo trim($output, $seperator);
+
+			}
+
+		?></p>
+
 		<?php the_content(); ?>
 	</article>
-	
+
 	<?php endwhile;
 
 	else :
